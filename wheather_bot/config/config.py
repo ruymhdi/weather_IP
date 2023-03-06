444c027c3f0aaa4fc1feff0075bed09d96fc4abf
Rasulov <108322250+ruymhdi@users.noreply.github.com>
2023-03-06 16:31:43 +0500

    Add files via upload

---
+++ wheather_bot/config/config.py
@@ -0,0 +1,9 @@
+BOT_API_TOKEN = '6248381560:AAFDPUGWHEUP3p7xb_kNfKOE5QRVsF2D_Js'
+WEATHER_API_KEY = '1c19934eedb501f53c0bcc802a78e40a'
+
+CURRENT_WEATHER_API_CALL = (
+                'https://api.openweathermap.org/data/2.5/weather?'
+                'lat={latitude}&lon={longitude}&'
+                'appid=' + WEATHER_API_KEY + '&units=metric'
+)
+
